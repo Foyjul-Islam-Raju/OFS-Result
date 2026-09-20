d1d9c7d05ed174ab74414e6db9f4fe894ec39f4f
Foyjul Islam Raju <raju.ju.cse@gmail.com>
2026-09-21 01:04:48 +0600

    Fix static files

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
-# Vercel build step: install dependencies and collect static files.
 set -e
-python3 -m pip install --upgrade pip
-python3 -m pip install -r requirements.txt
 python3 manage.py collectstatic --noinput --clear
